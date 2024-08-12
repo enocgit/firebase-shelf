38f78bfe4ca904d51b01aaa64b6b8ae5f94f864c
Enoch Ansah <enocansah@yahoo.com>
2024-08-12 23:03:55 +0000

    add FirebaseAuthService file

---
+++ FirebaseAuthService.ts
@@ -0,0 +1,43 @@
+import firebase from "./FirebaseConfig";
+
+const auth = firebase.auth();
+
+const registerUser = (email: string, password: string) => {
+  return auth.createUserWithEmailAndPassword(email, password);
+};
+
+const loginUser = (email: string, password: string) => {
+  return auth.signInWithEmailAndPassword(email, password);
+};
+
+const logoutUser = () => {
+  return auth.signOut();
+};
+
+const sendPasswordResetEmail = (email: string) => {
+  return auth.sendPasswordResetEmail(email);
+};
+
+const loginWithGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return auth.signInWithPopup(provider);
+};
+
+const subscribeToAuthChanges = (
+  callback: (user: firebase.User | null) => void,
+) => {
+  auth.onAuthStateChanged((user) => {
+    callback(user);
+  });
+};
+
+const FirebaseAuthService = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  sendPasswordResetEmail,
+  loginWithGoogle,
+  subscribeToAuthChanges,
+};
+
+export default FirebaseAuthService;
